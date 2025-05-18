5fd504057d3b92b6eb17434fb5ea61826524d88a
Enzo Lizama <lizama.enzo@gmail.com>
2025-05-18 16:28:36 -0500

    Refactor HttpClientFactory to remove expect modifier and add compiler options for expect-actual classes

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -18,6 +18,13 @@ kotlin {
             }
         }
     }
+    targets.configureEach {
+        compilations.configureEach {
+            compileTaskProvider.get().compilerOptions {
+                freeCompilerArgs.add("-Xexpect-actual-classes")
+            }
+        }
+    }
     iosX64()
     iosArm64()
     iosSimulatorArm64()
--- shared/src/commonMain/kotlin/com/enzoftware/translatorapp/translate/data/remote/HttpClientFactory.kt
+++ shared/src/commonMain/kotlin/com/enzoftware/translatorapp/translate/data/remote/HttpClientFactory.kt
@@ -3,6 +3,5 @@ package com.enzoftware.translatorapp.translate.data.remote
 import io.ktor.client.HttpClient
 
 expect class HttpClientFactory {
-    expect fun create(): HttpClient
-
+    fun create(): HttpClient
 }
