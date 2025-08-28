9fc50baf2e086cae0527117967510a4a0161a03a
Enzo Lizama <lizama.enzo@gmail.com>
2025-08-28 18:25:13 -0500

    Implement clipboard functionality and enhance TranslateTextField interactions

--- androidApp/src/main/java/com/enzoftware/translatorapp/android/translate/presentation/TranslateScreen.kt
+++ androidApp/src/main/java/com/enzoftware/translatorapp/android/translate/presentation/TranslateScreen.kt
@@ -1,20 +1,31 @@
 package com.enzoftware.translatorapp.android.translate.presentation
 
+import android.content.ClipData
+import android.widget.Toast
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
+import com.enzoftware.translatorapp.android.R
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.ClipEntry
+import androidx.compose.ui.platform.LocalClipboard
+import androidx.compose.ui.platform.LocalClipboardManager
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.unit.dp
 import com.enzoftware.translatorapp.android.translate.presentation.components.LanguageDropDown
 import com.enzoftware.translatorapp.android.translate.presentation.components.SwapLanguageButton
+import com.enzoftware.translatorapp.android.translate.presentation.components.TranslateTextField
 import com.enzoftware.translatorapp.translate.presentation.TranslateEvent
 import com.enzoftware.translatorapp.translate.presentation.TranslateState
 
@@ -22,8 +33,9 @@ import com.enzoftware.translatorapp.translate.presentation.TranslateState
 fun TranslateScreen(
     state: TranslateState,
     onEvent: (TranslateEvent) -> Unit,
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
 ) {
+    val context = LocalContext.current
     Scaffold(
         floatingActionButton = {
 
@@ -73,6 +85,42 @@ fun TranslateScreen(
                         }
                     )
                 }
+
+            }
+
+            item {
+                val clipboardManager = LocalClipboardManager.current
+                val keyboardController = LocalSoftwareKeyboardController.current
+
+                TranslateTextField(
+                    fromText = state.fromText,
+                    toText = state.toText,
+                    isTranslating = state.isTranslating,
+                    fromLanguage = state.fromLanguage,
+                    toLanguage = state.toLanguage,
+                    onTranslateClick = {
+                        onEvent(TranslateEvent.Translate)
+                    },
+                    onTextChange = { text ->
+                        onEvent(TranslateEvent.ChangeTranslationText(text))
+                    },
+                    onCopyClick = { text ->
+                        keyboardController?.hide()
+                        clipboardManager.setText(buildAnnotatedString { append(text) })
+                        Toast.makeText(context, context.getString(R.string.translation_copied), Toast.LENGTH_SHORT)
+                            .show()
+                    },
+                    onCloseClick = {
+                        onEvent(TranslateEvent.CloseTranslation)
+                    },
+                    onSpeakerClick = {
+
+                    },
+                    onTextFieldClick = {
+                        onEvent(TranslateEvent.EditTranslation)
+                    },
+                    modifier = Modifier.fillMaxWidth(),
+                )
             }
         }
     }
