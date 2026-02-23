e85f274c5bcb9b75c3429a715208869b89ada3b6
Enzo Lizama <lizama.enzo@gmail.com>
2026-02-23 00:41:07 -0500

    feat: Implement `TranslateTextField` component with gradient surface, language display, progress button, and text-to-speech functionality, integrating it into the `TranslateScreen`.

---
+++ iosApp/iosApp/translate/presentation/components/LanguageDisplay.swift
@@ -0,0 +1,29 @@
+//
+//  LanguageDisplay.swift
+//  iosApp
+//
+//  Created by Enzo Lizama on 22/02/26.
+//  Copyright © 2026 orgName. All rights reserved.
+//
+
+import SwiftUI
+import shared
+struct LanguageDisplay: View {
+    var language: UiLanguage
+    
+    var body: some View {
+        HStack {
+            SmallLanguageIcon(language: language)
+                .padding(.trailing, 6)
+            Text(language.language.name)
+                .foregroundColor(.lightBlue)
+        }
+        
+    }
+}
+
+#Preview {
+    LanguageDisplay(
+        language: UiLanguage(language: .english, imagePath: "english")
+    )
+}
